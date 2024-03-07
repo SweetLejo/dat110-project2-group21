998ceea749cabfedb649aca653743dd08ab70ab2
leo <heldal.leonard@gmail.com>
2024-03-07 16:06:42 +0100

    changes and whatever

---
+++ src/main/java/no/hvl/dat110/messagetransport/MessageConfig.java
@@ -0,0 +1,9 @@
+package no.hvl.dat110.messagetransport;
+
+public class MessageConfig {
+
+	public static final int SEGMENTSIZE = 128;
+	
+	public static final int MESSAGINGPORT = 8080;
+	public static final String MESSAGINGHOST = "127.0.0.1";
+}
